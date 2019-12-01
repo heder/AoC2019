d4155c0727de0fa29879943046f69a73d2367258
Thomas Heder <thomas.heder@gmail.com>
2019-12-01 17:41:05 +0100

    Day1

---
+++ Day1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("in.txt").ToList();
+            int totalFuel = lines.Sum(f => CalcFuel(Convert.ToInt32(f)));
+
+            Console.WriteLine(totalFuel);
+            Console.ReadLine();
+        }
+
+        static int CalcFuel(int mass)
+        {
+            return Convert.ToInt32(Math.Floor((decimal)mass / 3m) - 2);
+        }
+    }
+}
---
+++ Day1.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("in.txt");
+            List<int> input = new List<int>();
+            int totalFuel = 0;
+
+            foreach (var item in lines)
+            {
+                input.Add(Convert.ToInt32(item));
+            }
+
+            foreach (var mass in input)
+            {
+                int localFuel = CalcFuel(mass);
+                int massAdded = localFuel;
+
+                while (true)
+                {
+                    massAdded = CalcFuel(massAdded);
+
+                    if (massAdded <= 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        localFuel += massAdded;
+                    }
+                }
+
+                totalFuel += localFuel;
+            }
+
+            Console.WriteLine(totalFuel);
+            Console.ReadLine();
+        }
+
+        static int CalcFuel(int mass)
+        {
+            return Convert.ToInt32(Math.Floor((decimal)mass / (decimal)3) - 2);
+        }
+    }
+}
