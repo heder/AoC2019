c163aa4d8bcb9d889c5d02bdca0ed9bd6d750c12
Thomas Heder <thomas.heder@gmail.com>
2019-12-05 07:26:36 +0100

    Day5

--- Day5.2/Program.cs
+++ Day5.2/Program.cs
@@ -37,7 +37,7 @@ namespace Day5_2
                 //    p1val = (p1mode == 0) ? ram[ram[pc + 1]] : ram[pc + 1];
                 //}
 
-                if (opcode == 1 || opcode == 2)
+                if (opcode == 1 || opcode == 2 || opcode == 5 || opcode == 6 || opcode == 7 || opcode == 8)
                 {
                     p1val = (p1mode == 0) ? ram[ram[pc + 1]] : ram[pc + 1];
                     p2val = (p2mode == 0) ? ram[ram[pc + 2]] : ram[pc + 2];
@@ -74,7 +74,55 @@ namespace Day5_2
                         pc += 2;
                         break;
 
+                    case 5:
+                        if (p1val != 0)
+                        {
+                            pc = p2val;
+                        }
+                        else
+                        {
+                            pc += 4;
+                        }
+                        break;
+
+                    case 6:
+                        if (p1val == 0)
+                        {
+                            pc = p2val;
+                        }
+                        else
+                        {
+                            pc += 4;
+                        }
+                        break;
+
+                    case 7:
+                        if (p1val < p2val)
+                        {
+                            ram[ram[pc + 3]] = 1;
+                        }
+                        else
+                        {
+                            ram[ram[pc + 3]] = 0;
+                        }
+                        pc += 4;
+                        break;
+
+                    case 8:
+                        if (p1val == p2val)
+                        {
+                            ram[ram[pc + 3]] = 1;
+                        }
+                        else
+                        {
+                            ram[ram[pc + 3]] = 0;
+                        }
+                        pc += 4;
+                        break;
+
                     default:
+                        Console.WriteLine($"Invalid instruction {instruction} at pc {pc}");
+                        Console.ReadKey();
                         break;
                 }
 
