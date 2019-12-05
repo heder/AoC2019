efab61c2539445e81c1d8a008f01aa368e105249
Thomas Heder <thomas.heder@gmail.com>
2019-12-05 19:29:35 +0100

    Day6

---
+++ Day6.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
