5d594b3cdbde3fb5809463969512c65ba902608d
Thomas Heder <thomas.heder@sogeti.com>
2019-12-10 10:36:15 +0100

    Day10

--- Day10.1/Program.cs
+++ Day10.1/Program.cs
@@ -12,7 +12,7 @@ namespace Day10._1
     {
         class Asteroid
         {
-            public Point2D coord { get; set; }
+            public PointF coord { get; set; }
             public int CanSee { get; set; }
         }
 
@@ -30,11 +30,13 @@ namespace Day10._1
                 {
                     if (inputString[y][x] == '#')
                     {
-                        asteroids.Add(new Asteroid() { coord = new Point2D(x, y), CanSee = 0 });
+                        asteroids.Add(new Asteroid() { coord = new PointF(x, y), CanSee = 0 });
                     }
                 }
             }
 
+
+            List<Asteroid> blockingAsteroids = new List<Asteroid>();
 
             // Loop all asteroids
             foreach (var from in asteroids)
@@ -43,7 +45,7 @@ namespace Day10._1
                 var others = asteroids.Except(new List<Asteroid>() { from });
                 foreach (var to in others)
                 {
-                    Line2D lineOfSight = new Line2D(from.coord, to.coord);
+                    //Line2D lineOfSight = new Line2D(from.coord, to.coord);
 
                     var blockers = asteroids.Except(new List<Asteroid>() { from, to });
                     bool blockerExists = false;
@@ -52,9 +54,10 @@ namespace Day10._1
 
                         //var tol = Angle.FromDegrees(0.5);
 
-                        Line2D lineToPotentialBlocker = new Line2D(from.coord, blocker.coord);
-                        blockerExists = lineOfSight.IsParallelTo(lineToPotentialBlocker) && lineOfSight.Length <= lineToPotentialBlocker.Length;
-
+                        //Line2D lineToPotentialBlocker = new Line2D(from.coord, blocker.coord);
+                        //blockerExists = lineOfSight.IsParallelTo(lineToPotentialBlocker) && lineOfSight.Length <= lineToPotentialBlocker.Length && lineOfSight.IntersectWith(lineToPotentialBlocker);
+                        //blockerExists = lineOfSight.IntersectWith(lineToPotentialBlocker);
+                        blockerExists = IsOnLine(from.coord, to.coord, blocker.coord);
                         if (blockerExists == true)
                         {
                             break;
@@ -71,6 +74,26 @@ namespace Day10._1
             var max = asteroids.Max(f => f.CanSee);
             var res = asteroids.Where(g => g.CanSee == max);
 
+
+
+            static bool IsOnLine(PointF pt1, PointF pt2, PointF pt, double epsilon = 0.001)
+            {
+                if (pt.X - Math.Max(pt1.X, pt2.X) > epsilon ||
+                    Math.Min(pt1.X, pt2.X) - pt.X > epsilon ||
+                    pt.Y - Math.Max(pt1.Y, pt2.Y) > epsilon ||
+                    Math.Min(pt1.Y, pt2.Y) - pt.Y > epsilon)
+                    return false;
+
+                if (Math.Abs(pt2.X - pt1.X) < epsilon)
+                    return Math.Abs(pt1.X - pt.X) < epsilon || Math.Abs(pt2.X - pt.X) < epsilon;
+                if (Math.Abs(pt2.Y - pt1.Y) < epsilon)
+                    return Math.Abs(pt1.Y - pt.Y) < epsilon || Math.Abs(pt2.Y - pt.Y) < epsilon;
+
+                double x = pt1.X + (pt.Y - pt1.Y) * (pt2.X - pt1.X) / (pt2.Y - pt1.Y);
+                double y = pt1.Y + (pt.X - pt1.X) * (pt2.Y - pt1.Y) / (pt2.X - pt1.X);
+
+                return Math.Abs(pt.X - x) < epsilon || Math.Abs(pt.Y - y) < epsilon;
+            }
 
         }
 
