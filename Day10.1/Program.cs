ed2519d2219de57222fc6572787fe8dcac04da8e
Thomas Heder <thomas.heder@sogeti.com>
2019-12-10 10:55:53 +0100

    Day10

--- Day10.1/Program.cs
+++ Day10.1/Program.cs
@@ -45,21 +45,18 @@ namespace Day10._1
                 var others = asteroids.Except(new List<Asteroid>() { from });
                 foreach (var to in others)
                 {
-                    //Line2D lineOfSight = new Line2D(from.coord, to.coord);
-
                     var blockers = asteroids.Except(new List<Asteroid>() { from, to });
+
+                    // Sort the blockers list according to distance, so that the blockingAsteroids list contains the closest ones.
+                    blockers.OrderBy(f => new Line2D(new Point2D(from.coord.X, from.coord.Y), new Point2D(f.coord.X, f.coord.Y)).Length);
+
                     bool blockerExists = false;
                     foreach (var blocker in blockers)
                     {
-
-                        //var tol = Angle.FromDegrees(0.5);
-
-                        //Line2D lineToPotentialBlocker = new Line2D(from.coord, blocker.coord);
-                        //blockerExists = lineOfSight.IsParallelTo(lineToPotentialBlocker) && lineOfSight.Length <= lineToPotentialBlocker.Length && lineOfSight.IntersectWith(lineToPotentialBlocker);
-                        //blockerExists = lineOfSight.IntersectWith(lineToPotentialBlocker);
                         blockerExists = IsOnLine(from.coord, to.coord, blocker.coord);
                         if (blockerExists == true)
                         {
+                            blockingAsteroids.Add(blocker);
                             break;
                         }
                     }
@@ -76,21 +73,21 @@ namespace Day10._1
 
 
 
-            static bool IsOnLine(PointF pt1, PointF pt2, PointF pt, double epsilon = 0.001)
+            static bool IsOnLine(PointF start, PointF end, PointF pt, double epsilon = 0.001)
             {
-                if (pt.X - Math.Max(pt1.X, pt2.X) > epsilon ||
-                    Math.Min(pt1.X, pt2.X) - pt.X > epsilon ||
-                    pt.Y - Math.Max(pt1.Y, pt2.Y) > epsilon ||
-                    Math.Min(pt1.Y, pt2.Y) - pt.Y > epsilon)
+                if (pt.X - Math.Max(start.X, end.X) > epsilon ||
+                    Math.Min(start.X, end.X) - pt.X > epsilon ||
+                    pt.Y - Math.Max(start.Y, end.Y) > epsilon ||
+                    Math.Min(start.Y, end.Y) - pt.Y > epsilon)
                     return false;
 
-                if (Math.Abs(pt2.X - pt1.X) < epsilon)
-                    return Math.Abs(pt1.X - pt.X) < epsilon || Math.Abs(pt2.X - pt.X) < epsilon;
-                if (Math.Abs(pt2.Y - pt1.Y) < epsilon)
-                    return Math.Abs(pt1.Y - pt.Y) < epsilon || Math.Abs(pt2.Y - pt.Y) < epsilon;
+                if (Math.Abs(end.X - start.X) < epsilon)
+                    return Math.Abs(start.X - pt.X) < epsilon || Math.Abs(end.X - pt.X) < epsilon;
+                if (Math.Abs(end.Y - start.Y) < epsilon)
+                    return Math.Abs(start.Y - pt.Y) < epsilon || Math.Abs(end.Y - pt.Y) < epsilon;
 
-                double x = pt1.X + (pt.Y - pt1.Y) * (pt2.X - pt1.X) / (pt2.Y - pt1.Y);
-                double y = pt1.Y + (pt.X - pt1.X) * (pt2.Y - pt1.Y) / (pt2.X - pt1.X);
+                double x = start.X + (pt.Y - start.Y) * (end.X - start.X) / (end.Y - start.Y);
+                double y = start.Y + (pt.X - start.X) * (end.Y - start.Y) / (end.X - start.X);
 
                 return Math.Abs(pt.X - x) < epsilon || Math.Abs(pt.Y - y) < epsilon;
             }
