98e888b82ce41feaa87cd28af3545885faa5f801
Thomas Heder <thomas.heder@gmail.com>
2019-12-13 07:38:06 +0100

    Day12

--- Day13.2/Program.cs
+++ Day13.2/Program.cs
@@ -11,14 +11,17 @@ namespace Day13._1
         {
             long[] input = File.ReadAllLines("in.txt")[0].Split(',').Select(f => Convert.ToInt64(f)).ToArray();
 
-            const int SIZE = 50;
+            const int XSIZE = 50;
+            const int YSIZE = 50;
 
-            int[,] grid = new int[SIZE, SIZE];
+            int[,] grid = new int[XSIZE, YSIZE];
 
             input[0] = 2; // Free to play
             Intcode.CPU cpu = new Intcode.CPU(input);
 
             int score = 0;
+            int ballPos = 0;
+            int paddlePos = 0;
 
             int iterations = 0;
             while (cpu.State != Intcode.CpuState.STOPPED)
@@ -30,24 +33,38 @@ namespace Day13._1
 
                 int ii = 0;
 
-                if (iterations > 1000)
+                //if (iterations > 1000)
+                //{
+                //    var joy = Console.ReadKey();
+
+
+                //    if (joy.KeyChar == 'a')
+                //    {
+                //        ii = -1;
+                //    }
+                //    if (joy.KeyChar == 'd')
+                //    {
+                //        ii = 1;
+                //    }
+                //    if (joy.KeyChar == ' ')
+                //    {
+                //        ii = 0;
+                //    }
+                //}
+
+                if (paddlePos > ballPos)
                 {
-                    var joy = Console.ReadKey();
-
-
-                    if (joy.KeyChar == 'a')
-                    {
-                        ii = -1;
-                    }
-                    if (joy.KeyChar == 'd')
-                    {
-                        ii = 1;
-                    }
-                    if (joy.KeyChar == ' ')
-                    {
-                        ii = 0;
-                    }
+                    ii = -1;
                 }
+                if (paddlePos < ballPos)
+                {
+                    ii = 1;
+                }
+                if (paddlePos == ballPos)
+                {
+                    ii = 0;
+                }
+
 
                 cpu.Run(new long[1] { (long)ii });
                 if (cpu.State == Intcode.CpuState.OUTPUT_READY)
@@ -74,53 +91,65 @@ namespace Day13._1
                 else
                 {
                     grid[x, y] = c;
-                }
 
-                if (iterations > 1000)
-                {
-                    Console.Clear();
-                    for (int i = 0; i < SIZE; i++)
+                    if (c == 4)
                     {
-                        for (int j = 0; j < SIZE; j++)
-                        {
-                            //Console.Write(grid[i, j]);
-
-                            switch (grid[i, j])
-                            {
-                                case 0:
-                                    Console.Write(" ");
-                                    //b.SetPixel(j, i, Color.White);
-                                    break;
-
-                                case 1:
-                                    Console.Write("I");
-                                    //b.SetPixel(j, i, Color.White);
-                                    break;
-                                case 2:
-                                    Console.Write("X");
-                                    //b.SetPixel(j, i, Color.White);
-                                    break;
-                                case 3:
-                                    Console.Write("-");
-                                    //b.SetPixel(j, i, Color.White);
-                                    break;
-                                case 4:
-                                    Console.Write("o");
-                                    //b.SetPixel(j, i, Color.White);
-                                    break;
-
-                                default:
-                                    break;
-                            }
-
-
-                        }
-                        Console.WriteLine();
-                        
+                        ballPos = x;
                     }
 
-                    Console.WriteLine(score);
+                    if (c == 3)
+                    {
+                        paddlePos = x;
+                    }
                 }
+
+
+
+                //if (iterations > 1000)
+                //{
+                //    Console.Clear();
+                //    for (int i = 0; i < YSIZE; i++)
+                //    {
+                //        for (int j = 0; j < XSIZE; j++)
+                //        {
+                //            //Console.Write(grid[i, j]);
+
+                //            switch (grid[i, j])
+                //            {
+                //                case 0:
+                //                    Console.Write(" ");
+                //                    //b.SetPixel(j, i, Color.White);
+                //                    break;
+
+                //                case 1:
+                //                    Console.Write("I");
+                //                    //b.SetPixel(j, i, Color.White);
+                //                    break;
+                //                case 2:
+                //                    Console.Write("X");
+                //                    //b.SetPixel(j, i, Color.White);
+                //                    break;
+                //                case 3:
+                //                    Console.Write("H");
+                //                    //b.SetPixel(j, i, Color.White);
+                //                    break;
+                //                case 4:
+                //                    Console.Write("o");
+                //                    //b.SetPixel(j, i, Color.White);
+                //                    break;
+
+                //                default:
+                //                    break;
+                //            }
+
+
+                //        }
+                //        Console.WriteLine();
+                        
+                //    }
+
+                    Console.WriteLine(score);
+                //}
 
                 iterations++;
 
