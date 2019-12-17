a8b9766f7f88156902da248171e9b6ee62014fd7
Thomas Heder <thomas.heder@gmail.com>
2019-12-17 18:23:59 +0100

    Day17.2

--- Day17.1/Program.cs
+++ Day17.1/Program.cs
@@ -9,30 +9,22 @@ namespace Day17._1
     {
         static void Main(string[] args)
         {
-
             long[] input = File.ReadAllLines("in.txt")[0].Split(',').Select(f => Convert.ToInt64(f)).ToArray();
 
             const int SIZE = 100;
 
             int[,] grid = new int[SIZE, SIZE];
 
-            input[0] = 2; // Free to play
             Intcode.CPU cpu = new Intcode.CPU(input);
-
-            int score = 0;
-            int ballPos = 0;
-            int paddlePos = 0;
-
-            int iterations = 0;
             int output = 0;
 
             int x = 0;
             int y = 0;
 
-
             while (cpu.State != Intcode.CpuState.STOPPED)
             {
-                cpu.Run(new long[] { 1 });
+                cpu.input = new long[] { 1 };
+                cpu.Run();
                 if (cpu.State == Intcode.CpuState.OUTPUT_READY)
                 {
                     output = (int)cpu.Output;
@@ -53,18 +45,14 @@ namespace Day17._1
                         x++;
                         break;
                 }
-
-
             }
-
-            Console.ReadKey();
 
             int aggregate = 0;
             for (int i = 1; i < SIZE; i++)
             {
                 for (int j = 1; j < SIZE; j++)
                 {
-                    // Check if "#" is surronded by "#"
+                    // Check if "#" is surrounded by "#"
                     if (grid[i, j] == 35 && grid[i - 1, j] == 35 && grid[i + 1, j] == 35 && grid[i, j - 1] == 35 && grid[i, j + 1] == 35)
                     {
                         aggregate += i * j;
@@ -72,6 +60,7 @@ namespace Day17._1
                 }
             }
 
+            Console.WriteLine(aggregate);
             Console.ReadKey();
         }
     }
