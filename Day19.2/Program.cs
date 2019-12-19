9a6faebf30e24697ad1f363371d9ed63295b7b3f
Thomas Heder <thomas.heder@gmail.com>
2019-12-19 22:03:05 +0100

    Day19

--- Day19.2/Program.cs
+++ Day19.2/Program.cs
@@ -5,23 +5,19 @@ using System.Linq;
 
 namespace Day19._2
 {
-
     struct coord
     {
         public int x;
         public int y;
     }
 
-
     class Program
     {
-
-
         static void Main(string[] args)
         {
             long[] input = File.ReadAllLines("in.txt")[0].Split(',').Select(f => Convert.ToInt64(f)).ToArray();
 
-            const int SIZE = 1000;
+            const int SIZE = 1500;
 
             int[,] grid = new int[SIZE, SIZE];
             int output = 0;
@@ -31,8 +27,6 @@ namespace Day19._2
             {
                 for (int y = 0; y < SIZE; y++)
                 {
-                    Console.WriteLine($"{x},{y}");
-
                     cpu = new Intcode.CPU(input);
                     cpu.Input = new long[] { x, y };
                     cpu.Run();
@@ -48,63 +42,68 @@ namespace Day19._2
                 }
             }
 
+            int xmaxTractorBeam = 0;
+
+            // Find rightmost tractor beam position
+            for (int x = SIZE - 1; x > 0; x--)
+            {
+                for (int y = 0; y < SIZE; y++)
+                {
+                    if (grid[x, y] == 1)
+                    {
+                        Console.WriteLine($"First tractor beam grid is at xpos {x}");
+                        xmaxTractorBeam = x;
+                        break;
+                    }
+                }
+
+                if (xmaxTractorBeam > 0) break;
+            }
+
+            xmaxTractorBeam -= 100;
+
+            int lowx = 10000;
+            int lowy = 10000;
+
+            for (int xx = xmaxTractorBeam - 100; xx >= 0; xx--)
+            {
+                coord a = new coord() { x = xx, y = 0 };
+                coord b = new coord() { x = xx + 99, y = 0 };
+                coord c = new coord() { x = xx, y = 99 };
+                coord d = new coord() { x = xx + 99, y = 99 };
+
+                // Move ship down until all coordinates are inside beam
+                for (int i = 0; i < SIZE - 100; i++)
+                {
+                    a.y++;
+                    b.y++;
+                    c.y++;
+                    d.y++;
+
+                    if (grid[a.x, a.y] == 1 && grid[b.x, b.y] == 1 && grid[c.x, c.y] == 1 && grid[d.x, d.y] == 1)
+                    {
+                        Console.WriteLine($"Ship fits @ {a.x},{a.y}");
+                        if (a.x < lowx) { lowx = a.x; lowy = a.y; }
+                    }
+                }
+            }
 
             Bitmap bm = new Bitmap(SIZE, SIZE);
-
-
             for (int i = 0; i < SIZE; i++)
             {
                 for (int j = 0; j < SIZE; j++)
                 {
-                    if (grid[i,j] == 1)
+                    if (grid[i, j] == 1)
                         bm.SetPixel(i, j, Color.White);
                     else
                         bm.SetPixel(i, j, Color.Black);
                 }
             }
 
-
+            bm.SetPixel(lowx, lowy, Color.Red);
             bm.Save("output.png", System.Drawing.Imaging.ImageFormat.Png);
 
-            //// Find rightmost "1" pixel
-            //for (int x = SIZE - 1; x > 0; x--)
-            //{
-            //    for (int y = 0; y < length; y++)
-            //    {
-
-            //    }
-            //}
-
-
-
-            // a     b
-            // c     d
-
-            // Virtual corners, top right
-            coord a = new coord() { x = 0, y = SIZE - 100 - 1 };
-            coord b = new coord() { x = 0, y = SIZE - 1 };
-            coord c = new coord() { x = SIZE - 100 - 1, y = 99 };
-            coord d = new coord() { x = SIZE - 1, y = 99 };
-
-            // Move ship down until all coordinates are inside beam
-            for (int i = 0; i < SIZE; i++)
-            {
-                a.y++;
-                b.y++;
-                c.y++;
-                d.y++;
-
-                if (grid[d.x, d.y] == 1)
-                {
-                    Console.Write($"d inside @ {d.x},{d.y}");
-                }
-
-
-                Console.WriteLine();
-            }
-
-
-            //Console.WriteLine(aggregate);
+            Console.WriteLine($"{lowx},{lowy}");
             Console.ReadKey();
         }
     }
